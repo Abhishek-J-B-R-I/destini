e1c49b3c2f36c2509ab589c095ccea2e6adaa493
abhishek <abhishekmrjbr@outlook.com>
2023-08-11 20:12:05 +0530

    changing user interface because it's not look like what we think

--- lib/main.dart
+++ lib/main.dart
@@ -18,52 +18,60 @@ class _gameState extends State<game> {
     double width = MediaQuery.of(context).size.width;
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SafeArea(
-              child: Expanded(
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  height: height/1.7,
-                  width: width,
-                  child: Text('Story'),
 
-                  color: Colors.deepPurpleAccent,
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SafeArea(
+                child: Expanded(
+                  child: Container(
+
+                    padding: EdgeInsets.all(20),
+                    height: height/1.7,
+                    width: width,
+                    child: Text('Story'),
+
+                    color: Colors.deepPurpleAccent,
+
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height:height/50,
-            ),
-            Expanded(
-                child: FilledButton(
-              onPressed: () {
-                print('i am option 1');
-              },
-              child: Text('Option 1'),
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.green)),
-            )),
-            SizedBox(
-              height:height/50,
-            ),
-            Expanded(
-                child: FilledButton(
+              SizedBox(
+                height:height/50,
+              ),
+              Expanded(
+                  child: TextButton(
+                onPressed: () {
+                  print('i am option 1');
+                },
+                child: Text('Option 1',style: TextStyle(
+                  color: Colors.white
+                ),),
+                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red)),
+              )),
+              SizedBox(
+                height:height/50,
+              ),
+              Expanded(
+                  child: TextButton(
 
-                    onPressed: () {
-                      print('i am option2');
-                    },
-                    child: Text('Option 2'),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                      onPressed: () {
+                        print('i am option2');
+                      },
+                      child: Text('Option 2', style: TextStyle(color: Colors.white),),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blueAccent.shade400),
 
-                ),
+                  ),
 
-                )),
-            SizedBox(
-              height: height/60,
-            )
-          ],
+                  )),
+              SizedBox(
+                height: height/60,
+              )
+            ],
+          ),
         ),
       ),
     );
